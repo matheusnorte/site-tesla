f3d5112a094bc6ec555eae7b371d64a8bc41510e
matheusnorte <manortesul51@gmail.com>
2024-07-25 09:13:36 -0300

    primeiro projeto tesla

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let formulario = document.querySelector(".formulario")
+
+function aparecerformulario(){
+    formulario.style.left = "50%"
+}
+
+function desaparecerformulario(){
+    formulario.style.left = "-50%"
+}
